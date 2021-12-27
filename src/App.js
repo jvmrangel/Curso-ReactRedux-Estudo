0db9b231fecb153ee4f8c768b9e1b1564f1cf088
João Victor Rangel <55825561+jvmrangel@users.noreply.github.com>
2021-12-26 21:13:21 -0300

    Projeto simples funcional SEM redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+import Intervalo from './components/Intervalo';
+import Media from './components/Media';
+import Soma from './components/Soma';
+import Sorteio from './components/Sorteio';
+
+
 function App() {
+
+  const [min, setMin] = useState(100)
+  const [max, setMax] = useState(1000)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Exercício React-Redux (Simples)</h1>
+      <div className='linha'>
+        <Intervalo min={min} max={max}
+          onMinChanged={setMin} onMaxChanged={setMax}></Intervalo>
+      </div>
+      <div className='linha'>
+        <Media min={min} max={max}></Media>
+        <Soma min={min} max={max}></Soma>
+        <Sorteio min={min} max={max}></Sorteio>
+      </div>
     </div>
   );
 }
